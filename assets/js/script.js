bbf26013458208f339cc1410f7b29ce8af638f6a
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-07-12 23:07:58 +0200

    Create array of coordinates for select cities

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+//* Create array of coordinates for city buttons
+var cityBtnValues = 
+[
+  { city: "Washington, DC, US",
+    lat: "38.8951",
+    lon: "-77.0364"
+  },
+  { city: "New York City, US",
+    lat: "40.7143",
+    lon: "-74.006"
+  },
+  { city: "Berlin, DE",
+    lat: "52.5244",
+    lon: "13.4105"
+  },
+  { city: "Tokyo, JP",
+    lat: "35.6895",
+    lon: "139.6917"
+  },
+  { city: "Adelaide, AU",
+    lat: "-34.9205",
+    lon: "138.6006"
+  }
+];
+
+// TODO: Write initial functions
+function init() {
+    // initialization stuff here
+  }
+  
+//* Call initialization function
+  init();
