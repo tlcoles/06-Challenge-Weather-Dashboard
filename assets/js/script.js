7670371a5d304c30a0be4e12e761051185debc51
Tammi <21356324+tlcoles@users.noreply.github.com>
2022-07-27 17:36:31 +0200

    Fetch geodata and link to search (#13)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-//* Create array of coordinates for city buttons
+//* Create base variables
+const API_KEY = "bd04a337c9efdec64ce163c91978bf26";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/onecall?"
+const BASE_GEO_URL = "http://api.openweathermap.org/geo/1.0/direct?q"
+var lat;
+var lon;
+
 var cityBtnValues = 
 [
   { city: "Washington, DC, US",
@@ -23,47 +29,78 @@ var cityBtnValues =
   }
 ];
 
-// Fetch OpenWeather API data
+// Use Geocoding API to fetch city lat&lon data to pass to weatherFetch function
 
-function weatherFetch ( lat, lon ) {
-  const API_KEY = "bd04a337c9efdec64ce163c91978bf26";
-  const BASE_URL = "https://api.openweathermap.org/data/2.5/onecall?"
-  //
-  fetch (`${BASE_URL}lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${API_KEY}`)
+  function cityFetch (city) {
+  fetch (`${BASE_GEO_URL}=${city}&limit=2&appid=${API_KEY}`)
 
-  // Convert data to json
-  .then(function(resp) { 
+    // Convert data to json
+  .then(function (resp) {
     return resp.json(); 
-  }) 
+    })
 
-  // Call showWeather function
-  .then(function(data) {
-    console.log(data);
-    showWeather(data); 
+  // Pass the results to weatherFetch function
+  .then(function (data) {
+    console.log(data); 
+    let lat = data[0].lat;
+    let lon = data[0].lon;
+    weatherFetch( lat, lon ); 
+    })
+
+    .catch(function() {
+      // catch any errors
+    });
+  }
+
+// Listen to submit click, grab input city value, and pass value to cityFetch function
+
+  $("#citySearch").submit(function(event) {
+    event.preventDefault();
+    let city = $(".input-group-field").val();
+    $("#currentCity").text(city);
+    cityFetch (city)
   })
-  .catch(function() {
-    // catch any errors
-  });
-}
 
-// Execute fetch using default city (Berlin) 
-let lat = 52.5244;
-let lon = 13.4105;
-window.onload = function() {
-  weatherFetch( lat, lon ); 
-}
 
-function showWeather( data ) {
-	let celsiusTemp = Math.round(parseFloat(data.current.temp)-273.15);
-	let fahrenheit = Math.round(((parseFloat(data.current.temp)-273.15)*1.8)+32); 
+// Use OpenWeather API to fetch weather using lat&lon and use showWeather function to display on page
 
-  $("#currentCity").text(data.timezone);
-  $("#currentCityTemp").html(celsiusTemp + " &deg;C");
-  $("#currentCityWind").html(`<strong>Wind: </strong>`+ data.current.wind_speed);
-  $("#currentCityHumidity").html(`<strong>Humidity: </strong>`+ data.current.humidity + "&percnt;");
-  $("#currentCityUVI").html(`<strong>UVI: </strong>`+ data.current.uvi);
+  function weatherFetch (lat, lon) {
+    fetch (`${BASE_URL}lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${API_KEY}`)
 
-}
+    // Convert data to json
+    .then(function(resp) { 
+      return resp.json(); 
+    }) 
+
+    // Call showWeather function
+    .then(function(data) {
+      console.log(data);
+      showWeather(data); 
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
+
+// Execute fetch using default city (Berlin) on load
+
+  window.onload = function() {
+    let lat = 52.5244;
+    let lon = 13.4105;
+    weatherFetch( lat, lon ); 
+  }
+
+// Show weather data on the page
+
+  function showWeather( data ) {
+    let celsiusTemp = Math.round(parseFloat(data.current.temp)-273.15);
+    let fahrenheit = Math.round(((parseFloat(data.current.temp)-273.15)*1.8)+32); 
+
+    $("#currentCityTemp").html(celsiusTemp + " &deg;C");
+    $("#currentCityWind").html(`<strong>Wind: </strong>`+ data.current.wind_speed);
+    $("#currentCityHumidity").html(`<strong>Humidity: </strong>`+ data.current.humidity + "&percnt;");
+    $("#currentCityUVI").html(`<strong>UVI: </strong>`+ data.current.uvi);
+  }
 
 //! OpenWeather data
 // data.lat
