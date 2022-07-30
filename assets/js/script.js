9c89f4a31e29b2b4d6294517461d6ee2d35e6f90
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-07-30 07:31:05 +0200

    Remove unnecessary city array

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,30 +13,6 @@ const vhigh = [8,9,10]
 const extr = [11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
 
-let cityBtnValues = 
-[
-  { city: "Washington, DC, US",
-    lat: "38.8951",
-    lon: "-77.0364"
-  },
-  { city: "New York City, US",
-    lat: "40.7143",
-    lon: "-74.006"
-  },
-  { city: "Berlin, DE",
-    lat: "52.5244",
-    lon: "13.4105"
-  },
-  { city: "Tokyo, JP",
-    lat: "35.6895",
-    lon: "139.6917"
-  },
-  { city: "Adelaide, AU",
-    lat: "-34.9205",
-    lon: "138.6006"
-  }
-];
-
 // Listen for button click and pass ID value
 
 $(".cities").click(function () {
