bed53e496ff237e502c18428023c35b22ff028aa
Tammi <21356324+tlcoles@users.noreply.github.com>
2022-08-01 14:28:55 +0200

    29 show 5 day forecast for current selection (#30)
    * Remove unnecessary city array
    
    * Create forecast boxes
    
    * Place forecast in row

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,30 +13,6 @@ const vhigh = [8,9,10]
 const extr = [11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
 
-let cityBtnValues = 
-[
-  { city: "Washington, DC, US",
-    lat: "38.8951",
-    lon: "-77.0364"
-  },
-  { city: "New York City, US",
-    lat: "40.7143",
-    lon: "-74.006"
-  },
-  { city: "Berlin, DE",
-    lat: "52.5244",
-    lon: "13.4105"
-  },
-  { city: "Tokyo, JP",
-    lat: "35.6895",
-    lon: "139.6917"
-  },
-  { city: "Adelaide, AU",
-    lat: "-34.9205",
-    lon: "138.6006"
-  }
-];
-
 // Listen for button click and pass ID value
 
 $(".cities").click(function () {
@@ -89,7 +65,6 @@ $(".cities").click(function () {
 
     // Call showWeather function
     .then(function(data) {
-      console.log(data);
       let uvi = Math.round(data.current.uvi);
       showWeather(data);
       colorUVI (uvi);
@@ -102,7 +77,7 @@ $(".cities").click(function () {
 // Execute fetch using default city (Berlin) on load
 
   window.onload = function() {
-    cityFetch ("Berlin")
+    cityFetch ("Berlin");
   }
 
 // Show weather data on the page
@@ -111,17 +86,74 @@ $(".cities").click(function () {
     let celsiusTemp = Math.round(parseFloat(data.current.temp)-273.15);
     let fahrenheit = Math.round(((parseFloat(data.current.temp)-273.15)*1.8)+32); 
     let iconURL = "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png"  // Display weather icon
+    console.log(data);
+    let unixTime = data.daily[0].dt;
+    let date = new Date(unixTime*1000).toLocaleDateString("en-GB", {dateStyle: "full"});
+    console.log(date);
 
-
+    $("#currentCityDateTime").text(date);
     $('.weatherIcon').attr('src', iconURL);
     $("#currentCityTemp").html(celsiusTemp + " &deg;C");
     $("#currentCityWind").html(`<strong>Wind: </strong>`+ data.current.wind_speed);
     $("#currentCityHumidity").html(`<strong>Humidity: </strong>`+ data.current.humidity + "&percnt;");
     $("#currentCityUVI").html(`<strong>UVI: </strong>` + `<div id="colorme" class="">` + data.current.uvi + `</div>`);
+
+    let forecast5Day = [1,2,3,4,5];
+    forecast5Day.forEach((i) => {
+      console.log(i);
+      forecast(i)
+    });
+    //! Reduce repetition with an forEach or a for loop
+
+
+    function forecast(i) {
+      let dayCelsius = Math.round(parseFloat(data.daily[i].temp.day)-273.15);
+      let dayFahrenheit = Math.round(((parseFloat(data.daily[i].temp.day)-273.15)*1.8)+32); 
+      let smallURL = "https://openweathermap.org/img/wn/"
+
+      let dayCelsius1 = Math.round(parseFloat(data.daily[1].temp.day)-273.15);
+      let date1 = new Date(data.daily[1].dt*1000).toLocaleDateString("en-GB", {dateStyle: "medium"});
+      $("#fcDay1").html(date1)
+      $("#dayIcon1").attr('src', smallURL + data.daily[1].weather[0].icon + ".png");
+      $("#dayTemp1").html(dayCelsius1 + " &deg;C")
+      $("#dayWind1").html(`<strong>Wind: </strong>`+ data.daily[1].wind_speed);
+      $("#dayHumidity1").html(`<strong>Humidity: </strong>`+ data.daily[1].humidity + "&percnt;");
+
+      let dayCelsius2 = Math.round(parseFloat(data.daily[2].temp.day)-273.15);
+      let date2 = new Date(data.daily[2].dt*1000).toLocaleDateString("en-GB", {dateStyle: "medium"});
+      $("#fcDay2").html(date2)
+      $("#dayIcon2").attr('src', smallURL + data.daily[2].weather[0].icon + ".png");
+      $("#dayTemp2").html(dayCelsius2 + " &deg;C")
+      $("#dayWind2").html(`<strong>Wind: </strong>`+ data.daily[2].wind_speed);
+      $("#dayHumidity2").html(`<strong>Humidity: </strong>`+ data.daily[2].humidity + "&percnt;");
+
+      let dayCelsius3 = Math.round(parseFloat(data.daily[3].temp.day)-273.15);
+      let date3 = new Date(data.daily[3].dt*1000).toLocaleDateString("en-GB", {dateStyle: "medium"});
+      $("#fcDay3").html(date3)
+      $('#dayIcon3').attr('src', smallURL + data.daily[3].weather[0].icon + ".png");
+      $("#dayTemp3").html(dayCelsius3 + " &deg;C")
+      $("#dayWind3").html(`<strong>Wind: </strong>`+ data.daily[3].wind_speed);
+      $("#dayHumidity3").html(`<strong>Humidity: </strong>`+ data.daily[3].humidity + "&percnt;");
+
+      let dayCelsius4 = Math.round(parseFloat(data.daily[4].temp.day)-273.15);
+      let date4 = new Date(data.daily[4].dt*1000).toLocaleDateString("en-GB", {dateStyle: "medium"});
+      $("#fcDay4").html(date4)
+      $('#dayIcon4').attr('src', smallURL + data.daily[4].weather[0].icon + ".png");
+      $("#dayTemp4").html(dayCelsius4 + " &deg;C")
+      $("#dayWind4").html(`<strong>Wind: </strong>`+ data.daily[4].wind_speed);
+      $("#dayHumidity4").html(`<strong>Humidity: </strong>`+ data.daily[4].humidity + "&percnt;");
+
+      let dayCelsius5 = Math.round(parseFloat(data.daily[5].temp.day)-273.15);
+      let date5 = new Date(data.daily[5].dt*1000).toLocaleDateString("en-GB", {dateStyle: "medium"});
+      $("#fcDay5").html(date5)
+      $('#dayIcon5').attr('src', smallURL + data.daily[5].weather[0].icon + ".png");
+      $("#dayTemp5").html(dayCelsius5 + " &deg;C")
+      $("#dayWind5").html(`<strong>Wind: </strong>`+ data.daily[5].wind_speed);
+      $("#dayHumidity5").html(`<strong>Humidity: </strong>`+ data.daily[5].humidity + "&percnt;");
+    }
+
   }
 
-
-  
 
 // Check the UVI value and change its color appropriately
 function colorUVI (uvi) {
